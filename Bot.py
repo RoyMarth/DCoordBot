c8b4e7697a750d8062ebd28ae6cdacd54e710b7f
Matt Slencsak <mslencsak@gmail.com>
2022-07-06 21:26:48 -0400

    Bot Does Things

--- Bot.py
+++ Bot.py
@@ -5,11 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to discord')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'{client.user} has connected to discord' f'{guild.name}(id: {guild.id})')
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to a Brave New Realm!')
 client.run(TOKEN)
